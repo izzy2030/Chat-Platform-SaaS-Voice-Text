d3be855c89855a1967d4e320fd9505cfa0628687
Queens Mobile <queensmobile11@gmail.com>
2025-11-07 20:58:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4841799393-33b1c",
+  "appId": "1:329746797501:web:3dcf7f023b98cb3395f8d4",
+  "apiKey": "AIzaSyC1V0qDgFvuvjUP2_aPuiQ0JbnxydgQcuQ",
+  "authDomain": "studio-4841799393-33b1c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "329746797501"
+};
