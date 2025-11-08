f925f5dde685efe6c17da5adcc27f53196ebc43c
Queens Mobile <queensmobile11@gmail.com>
2025-11-08 17:37:35 +0000

    Check all the values are correct. I couldn't find the NEXT_PUBLIC_FIREBA

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,6 +4,5 @@ export const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
